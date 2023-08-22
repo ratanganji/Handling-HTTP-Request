ef4712229ea6ce4d222b39d90d7fe6058f149b6b
rathan <rathanganji99@gmail.com>
2023-08-22 13:12:34 +0000

    Handling HTTP Request

---
+++ myapp/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+app.get(`/`, (request, response) => {
+  response.send("hello world");
+});
+
+app.get("/date", (request, response) => {
+  let date = new Date();
+  response.send(`Today's date is ${date}`);
+});
+
+app.listen(3000);
